7e51256a8ae4e425c8bfc498a0b6671c5fe91a1b
Nozomi Imai <nimai@c3r2s6.42urduliz.com>
2023-02-07 18:59:48 +0100

    .

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: nimai <nimai@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 15:15:49 by nimai             #+#    #+#             */
-/*   Updated: 2023/02/07 18:11:54 by nimai            ###   ########.fr       */
+/*   Updated: 2023/02/07 18:59:42 by nimai            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,6 @@ void	push_swap(int ac, char **av)
 	t_stack	a;//stack a, put all av here
 	t_stack	b;//stack b, should be empty
 
-	a = get_list(ac, av);
+	a = malloc(get_list(ac, av));
 	
 }
