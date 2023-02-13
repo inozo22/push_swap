fdca75d1d1f89ef44637cec024c5a628d6c030e4
nimai <nimai@student.42urduliz.com>
2023-02-13 21:59:34 +0100

    revived

--- push_swap.c
+++ push_swap.c
@@ -12,16 +12,17 @@
 
 #include "push_swap.h"
 
-t_boxes	*search_head(t_boxes *dummy)//I think I should eliminate this
-{//think about it later
+t_boxes	*search_head(t_boxes *dummy)
+{
 	t_boxes	*ret;
 
 	ret = dummy;
-	while (ret->next != dummy)
+	printf("::::::::::\nprev of dummy before while: %p\n", dummy->prev);
+	while (ret->prev != dummy)
 	{
-		ret = ret->next;
+		ret = ret->prev;
 	}
-	printf("point of ret: %p\n", ret->prev);
+	printf("point of ret: %p\n", ret);
 	return (ret);
 }
 
@@ -31,12 +32,12 @@ void	add_box(t_boxes *dummy, int num)
 	t_boxes	*tmp;
 	t_boxes	*head;
 
+	printf("********\npoint of dummy before search head: %p\n", dummy);
 	head = search_head(dummy);
 	new = malloc(sizeof(t_boxes));
 	/* if (!new)
 		return (ps_error());//Here, I have to free the dummy too(clearlist?) */
 	tmp = dummy->prev;
-	//printf("point of dummy's prev: %p\npoint of dummy: %p\n", dummy->prev, dummy);
 	new->value = num;
 	new->next = dummy;
 	new->prev = tmp;
@@ -44,9 +45,7 @@ void	add_box(t_boxes *dummy, int num)
 	dummy->next = head;
 	printf("point of head: %p\n", head);
 	printf("prev of dummy: %p\nnext of dummy: %p\npoint of dummy: %p\n", dummy->prev, dummy->next, dummy);
-	//printf("value of new: %d\npoint of new: %p\nnext of new: %p\nprev of new: %p\n",\
-	 new->value, new, new->next, new->prev);
-	//printf("point of dummy's prev: %p\npoint of dummy's next: %p\npoint of dummy: %p\n", dummy->prev, dummy->next, dummy);
+
 }
 
 t_boxes	*make_dummy(void)
@@ -59,8 +58,6 @@ t_boxes	*make_dummy(void)
 	dummy->value = -1;//This value it will not be seen, so it's ok whatever I put
 	dummy->next = dummy;
 	dummy->prev = dummy;//This is the dummy box, that 
-	//printf("value of dummy: %d\npoint of dummy: %p\nnext of dummy: %p\nprev of dummy: %p\n",\
-	 dummy->value, dummy, dummy->next, dummy->prev);
 	return (dummy);
 }
 
@@ -94,8 +91,8 @@ void	main(void)
 	t_boxes *dummy;
 
 	dummy = make_dummy();
-	add_box(dummy, 123);/* 
-	add_box(dummy, 234);
+	add_box(dummy, 123); 
+	add_box(dummy, 234);/*
 	add_box(dummy, 345);
 	printf("::::::::\nvalue of dummy: %d\npoint of dummy: %p\n", dummy->value, dummy);
 	dummy = dummy->prev;
