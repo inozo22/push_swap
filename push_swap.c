7f5998d509326f16ea5d7979067622674a14411a
nimai <nimai@c4r1s5.42urduliz.com>
2023-02-18 11:56:35 +0100

    is_sorted fixed

--- push_swap.c
+++ push_swap.c
@@ -6,21 +6,24 @@
 /*   By: nimai <nimai@student.42urduliz.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 13:02:37 by nimai             #+#    #+#             */
-/*   Updated: 2023/02/18 11:26:55 by nimai            ###   ########.fr       */
+/*   Updated: 2023/02/18 11:52:13 by nimai            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-long	is_sorted(t_boxes *stack_a)
+bool	is_sorted(t_boxes *stack_a)
 {
-	while (stack_a->next->value != -1)
+	t_boxes	*tmp;
+
+	tmp = stack_a;
+	while (tmp->next->value != -1)
 	{
-		if (!(stack_a->next->value < stack_a->next->next->value))
-			return (0);
-		stack_a = stack_a->next;
+		if (tmp->value > tmp->next->value)
+			return (false);
+		tmp = tmp->next;
 	}
-	return (1);
+	return (true);
 }
 
 t_boxes	*search_head(t_boxes *dummy)//look for the first box that is not a dummy
@@ -39,19 +42,19 @@ void	add_box(t_boxes *dummy, long num)
 {
 	t_boxes	*new;
 	t_boxes	*tmp;
-	t_boxes	*head;
+//	t_boxes	*head;
 
-	head = search_head(dummy);
+//	head = search_head(dummy);
 	new = malloc(sizeof(t_boxes));
 	if (!new)
 		exit (ps_error("fail allocate new"));//later I have to omid this str
 	tmp = dummy->prev;
-	tmp->next = new;//
+	tmp->next = new;
 	new->value = num;
 	new->next = dummy;
-	new->prev = tmp;
 	dummy->prev = new;
-	dummy->next = head;
+//	dummy->next = head;
+	new->prev = tmp;
 }
 
 t_boxes	*make_dummy(void)
@@ -87,21 +90,19 @@ void	push_swap(int ac, char **av)
 	t_boxes		*stack_a;
 	t_boxes		*stack_b;
 	t_pushswap	*ps;
-	long	sortcheck = 0;
 	long i = 0;
 
 	ps = init_ps(ac, av);
 	stack_a = put_num(ps);
 	stack_b = make_dummy();
-	sortcheck = is_sorted(stack_a);
 	if (stack_a->next->value == -1)
 		return (all_free(stack_a, stack_b, ps));
-	else if (sortcheck == 1)
+	else if (is_sorted(stack_a))
 		printf("is sorted!\n");
 	else
 		printf("You have to sort\n");
-	printf("stack_a->next->value: %ld\n", stack_a->next->value);
-	while (i < ps->size)
+	printf("stack_a->value: %ld\n", stack_a->value);
+	while (stack_a->next->value == -1)
 	{
 		printf("value[%ld]: %ld\n", i, stack_a->next->value);
 		stack_a = stack_a->next;
