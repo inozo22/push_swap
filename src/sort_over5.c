5c1b93b7775312f438fe2b84a80ed04df3694866
nimai <nimai@c4r1s3.42urduliz.com>
2023-02-24 17:25:36 +0100

    add settle_top

--- src/sort_over5.c
+++ src/sort_over5.c
@@ -6,11 +6,48 @@
 /*   By: nimai <nimai@student.42urduliz.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/20 15:39:11 by nimai             #+#    #+#             */
-/*   Updated: 2023/02/24 13:11:18 by nimai            ###   ########.fr       */
+/*   Updated: 2023/02/24 17:25:00 by nimai            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
+
+void	settle_top(t_boxes *stack_a, t_boxes *stack_b, t_pushswap *ps, t_sorting *sort)
+{
+	long	i;
+
+	i = -1;
+	while ((++i + ps->a) < ps->b)
+	{
+		if (stack_b->prev->value == ps->b - i - 1)
+		{
+			cmd_reverse(stack_b);
+			sort->tmp[i] = RRB;	
+			ps->b++;
+		}
+		else if (stack_b->prev->prev->value == ps->b - i - 1)
+		{
+			cmd_reverse(stack_b);
+			sort->tmp[i] = RRB;
+			ps->b++;
+			cmd_reverse(stack_b);
+			sort->tmp[i] = RRB;
+			ps->b++;
+		}
+		if (stack_b->next->value == ps->b - i - 1)
+		{
+			cmd_push(stack_a, stack_b);
+			sort->tmp[i] = PA;	
+			ps->a++;
+		}
+	}
+	while (stack_a->next->value == ps->a)
+	{
+		cmd_rotate(stack_a);
+		sort->tmp[i] = RA;	
+		ps->a++;
+	}
+}
 
 void	settle_half(t_boxes *stack_a, t_boxes *stack_b, t_pushswap *ps, t_sorting *sort)
 {
@@ -21,7 +58,6 @@ void	settle_half(t_boxes *stack_a, t_boxes *stack_b, t_pushswap *ps, t_sorting *
 	i = 0;
 	size_b = 0;
 	tmp_a = stack_a;
-	printf("enter settle_half!!\n");
 	while (i < ps->size && size_b < (ps->size / 2))
 	{
 		if (tmp_a->next->value < (ps->size / 2))
@@ -43,10 +79,6 @@ void	sort_over5(t_boxes *stack_a, t_boxes *stack_b, t_pushswap *ps)
 
 
 	settle_half(stack_a, stack_b, ps, &sort);//set a half of a to b
-	while (ps->a != ps->size)
-	{
-
-	}
 	printf("--------------------------------\n");
 	printf("■   print stackA and stackB\t■\n■\tafter settle_half\t■\n");
 	printf("--------------------------------\n");
@@ -65,6 +97,12 @@ void	sort_over5(t_boxes *stack_a, t_boxes *stack_b, t_pushswap *ps)
 	printf("--------------------------------\n");
 	printf("■    stackA\tstackB\t\t■\n");
 	printf("--------------------------------\n");
+	while (ps->a != ps->size)
+	{
+		settle_top(stack_a, stack_b, ps, &sort);
+		return ;
+
+	}
 }
 
 /* int	main(int ac, char **av)
